e37d53cdf4a6edb5ba041cafcc3420b80ed926cc
Juan <juanandrescuevas14@gmail.com>
2023-11-07 22:11:34 -0400

    Add card combinations with similar attributes using backtracking

--- proyecto2/MundoChiquito.java
+++ proyecto2/MundoChiquito.java
@@ -1,7 +1,9 @@
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
 
 public class MundoChiquito{
     public static void main(String[] args) {
@@ -14,11 +16,24 @@ public class MundoChiquito{
         for (CartaMostro carta : cartas){
             grafo.add(carta);
         }
-        // TODO: Implementar backtracking para agregar las aristas al grafo en base a los atributos (nivel, tipo, poder)
+        // Agregamos las aristas al grafo
+        conectarCartasSemejantes(grafo, cartas);
+
+        // Obtenemos las combinaciones de cartas de monstruos con atributos en comun uno a uno
+        Set<List<CartaMostro>> combinaciones = backtracking(grafo);
+
+        // Imprimimos las combinaciones
+        System.out.println("Combinaciones de cartas de monstruos con atributos en comun uno a uno:");
+        for (List<CartaMostro> combinacion : combinaciones){
+            System.out.println(combinacion);
+        }
+        // Imprimimos la cantidad de combinaciones
+        System.out.println("Cantidad de combinaciones: " + combinaciones.size());
     }
 
     /**
      * Método que lee un archivo de cartas de monstruos y lo convierte en una lista
+     * de cartas de monstruos. Complejiad O(n) donde n es el número de cartas de monstruos
      * @param nombreArchivo Nombre del archivo a leer
      * @return Lista de cartas de monstruos
      */
@@ -52,7 +67,79 @@ public class MundoChiquito{
         }
     }
 
-    public static void backtracking(){
-        // TODO: Implementar backtracking para agregar las aristas al grafo en base a los atributos (nivel, tipo, poder)
+    /**
+     * Método que implementa para agregar las aristas al grafo
+     * Complejidad O(n^2) donde n es el número de cartas de monstruos
+     * @param grafo Grafo no dirigido
+     * @param cartas Lista de cartas de monstruos
+     */
+    public static void conectarCartasSemejantes(Graph<CartaMostro> grafo, List<CartaMostro> mazo){
+        for (CartaMostro carta : mazo){
+            for (CartaMostro c : mazo ){
+                if (!grafo.areConnected(c, carta) && atributosSemejantes(carta, c)){
+                    grafo.connect(c, carta);
+                }
+            }
+        }
+    }
+
+    /**
+     * Metodo que verifica si existe al menos un atributo igual entre dos cartas mostro
+     * Complejidad O(1)
+     * @param a Carta Mostro
+     * @param b Carta Mostro
+     * @return True si las dos cartas tienen por lo menos un atributo en comun, False en caso contrario.
+     */
+    public static boolean atributosSemejantes(CartaMostro a, CartaMostro b){
+        return a.getNivel() == b.getNivel() || a.getPoder() == b.getPoder() || a.getTipo().equals(b.getTipo());
+    }
+
+    /**
+     * Algoritmo de backtracking para encontrar las combinaciones de 3 cartas mostro con atributos en comun uno a uno.
+     * Complejidad O(n^3) donde n es el numero de cartas mostro.
+     * @param grafo
+     * @return Conjunto de combinaciones de 3 cartas mostro con atributos en comun uno a uno.
+     */
+    public static Set<List<CartaMostro>> backtracking(Graph<CartaMostro> grafo){
+        List<CartaMostro> solInicial = new ArrayList<>();
+        return backRec(grafo, solInicial);
+    }
+
+    /**
+     * Algoritmo de backtracking para encontrar las combinaciones de 3 cartas mostro con atributos en comun uno a uno.
+     * Complejidad O(n^3) donde n es el numero de cartas mostro.
+     * @param grafo
+     * @param sol
+     * @return Conjunto de combinaciones de 3 cartas mostro con atributos en comun uno a uno.
+     */
+    public static Set<List<CartaMostro>> backRec(Graph<CartaMostro> grafo, List<CartaMostro> sol){
+        Set<List<CartaMostro>> combinaciones = new HashSet<>();
+        if (sol.size() == 3){
+            combinaciones.add(sol);
+            return combinaciones;
+        }
+        for (CartaMostro carta : grafo.getAllVertices()){
+            if (esValida(sol, carta)){
+                sol.add(carta);
+                combinaciones.addAll(backRec(grafo, sol));
+                sol.remove(carta);
+            }
+        }
+        return combinaciones;
+    }
+
+    /**
+     * Metodo que verifica si una carta mostro puede ser agregada a una solucion.
+     * Complejidad O(1)
+     * @param sol
+     * @param carta
+     * @return True si la carta mostro puede ser agregada a la solucion, False en caso contrario.
+     */
+    public static boolean esValida(List<CartaMostro> sol, CartaMostro carta){
+        // Tomamos el ultimo elemento de la solucion
+        CartaMostro ultima = sol.get(sol.size() - 1);
+        // No es necesario verificar si la carta ya esta en la solucion, ya que la carta se puede repetir.
+        // Verificamos si la carta mostro es semejante a la ultima carta de la solucion
+        return atributosSemejantes(carta, ultima);
     }
 }
